3165417c3c83245f70470fba14536406a26af066
anand2810 <75320831+anand2810@users.noreply.github.com>
2020-12-06 09:30:05 +0530

    Add files via upload

---
+++ EventManagementSystems/utils/footer.php
@@ -0,0 +1,38 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>I</strong><span class = "small footerSubtext">nstitute of</span>
+                        <strong>A</strong><span class = "small footerSubtext">rt</span>
+                        <strong>D</strong><span class = "small footerSubtext">esign and</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        4th floor, CSE-Block, KL University
+                        &copy; Friends Events Venue & Catering, 2020.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    KLU Multimedia Design/Web Engineering<br>
+                    Creative Computing 2020-2021.<br>
+                    Anand Mani Sankar, 190030767
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
